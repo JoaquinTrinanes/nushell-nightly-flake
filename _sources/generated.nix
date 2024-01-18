8860e5b32fa7c4f4958bacbcbb29a7ecbf9d82f1
nvfetcher updater <noreply@github.com>
2024-01-18 14:10:55 +0000

    nushell: 55bf4d847fa3f2a41b746fd22b8a4355d9b1f108 → 19e76332fa5695d1158b0d681a1bf5a99c57d962

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -3,20 +3,20 @@
 {
   nushell = {
     pname = "nushell";
-    version = "55bf4d847fa3f2a41b746fd22b8a4355d9b1f108";
+    version = "19e76332fa5695d1158b0d681a1bf5a99c57d962";
     src = fetchFromGitHub {
       owner = "nushell";
       repo = "nushell";
-      rev = "55bf4d847fa3f2a41b746fd22b8a4355d9b1f108";
+      rev = "19e76332fa5695d1158b0d681a1bf5a99c57d962";
       fetchSubmodules = false;
-      sha256 = "sha256-U1yvjtHzpZ7FEIxAZwsiKpNnBgSwQLE92mAAEpDyIfo=";
+      sha256 = "sha256-nwStFCGn03gTBNceDavftVEKIB+gtQqbGo5+LdyQLSs=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./nushell-55bf4d847fa3f2a41b746fd22b8a4355d9b1f108/Cargo.lock;
+      lockFile = ./nushell-19e76332fa5695d1158b0d681a1bf5a99c57d962/Cargo.lock;
       outputHashes = {
         "reedline-0.28.0" = "sha256-Voc9yFpdcEE/EsxHmTO/w2I1jnXKPFg0qQiLQrx/hIM=";
       };
     };
-    date = "2024-01-17";
+    date = "2024-01-18";
   };
 }
