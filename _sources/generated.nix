e585c224a62a7d3a8a39c0a9e416f8b11c389a44
nvfetcher updater <noreply@github.com>
2024-02-01 13:11:46 +0000

    nushell: 16f3d9b4e1099399133a10b2dc25f19d2cbe8998 → 0b6f15b69e44aec839bedc5e0d3f49eb31ee64ff

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -3,16 +3,16 @@
 {
   nushell = {
     pname = "nushell";
-    version = "16f3d9b4e1099399133a10b2dc25f19d2cbe8998";
+    version = "0b6f15b69e44aec839bedc5e0d3f49eb31ee64ff";
     src = fetchFromGitHub {
       owner = "nushell";
       repo = "nushell";
-      rev = "16f3d9b4e1099399133a10b2dc25f19d2cbe8998";
+      rev = "0b6f15b69e44aec839bedc5e0d3f49eb31ee64ff";
       fetchSubmodules = false;
-      sha256 = "sha256-xh0OVezwRjAHK0UGN8fEWYghOEH/Z4Z2IR+tPHUrkMk=";
+      sha256 = "sha256-0pmtJQ8lEDG0Jov7u+3hbutP/uYuIVCII9ohKEClhA8=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./nushell-16f3d9b4e1099399133a10b2dc25f19d2cbe8998/Cargo.lock;
+      lockFile = ./nushell-0b6f15b69e44aec839bedc5e0d3f49eb31ee64ff/Cargo.lock;
       outputHashes = {
         "reedline-0.28.0" = "sha256-lI147QHAODbkt0F1CVbsnlXz5TUi15kJja/b+Z+50u4=";
       };
