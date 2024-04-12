581acb386c2509ec7e6d5d06b7729357137ba684
Joaquín Triñanes <hi@joaquint.io>
2024-04-13 00:25:39 +0200

    Format using nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -11,86 +11,104 @@
   };
 
   nixConfig = {
-    extra-substituters = [
-      "https://nushell-nightly.cachix.org"
-    ];
+    extra-substituters = [ "https://nushell-nightly.cachix.org" ];
     extra-trusted-public-keys = [
       "nushell-nightly.cachix.org-1:nLwXJzwwVmQ+fLKD6aH6rWDoTC73ry1ahMX9lU87nrc="
     ];
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      flake.overlays.default = final: prev: let
-        inherit (final.stdenv.hostPlatform) system;
-        packages = inputs.self.packages.${system};
-        inherit (inputs.nixpkgs) lib;
-      in {
-        inherit (packages) nushell nushellFull;
-        nushellPlugins = let pluginPkgs = lib.filterAttrs (name: _: lib.hasPrefix "nu_plugin_" name) packages; in lib.mapAttrs' (name: value: lib.nameValuePair (lib.removePrefix "nu_plugin_" name) value) pluginPkgs;
-      };
-
-      perSystem = {
-        pkgs,
-        lib,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-
-        devShells.default = pkgs.mkShell {
-          packages = builtins.attrValues {inherit (pkgs) npins;};
-        };
-        packages = let
-          plugins = ["formats" "gstat" "inc" "query"];
-          pluginPackageNames = map (p: "nu_plugin_${p}") plugins;
-          nushell = pkgs.callPackage ./nushell.nix {
-            doCheck = false;
-            inherit (pkgs.darwin.apple_sdk_11_0) Libsystem;
-            inherit (pkgs.darwin.apple_sdk_11_0.frameworks) AppKit Security;
-          };
+      flake.overlays.default =
+        final: prev:
+        let
+          inherit (final.stdenv.hostPlatform) system;
+          packages = inputs.self.packages.${system};
+          inherit (inputs.nixpkgs) lib;
         in
-          {
-            inherit nushell;
-            nushellFull = nushell.override {
-              additionalFeatures = default: (default ++ ["dataframe"]);
-            };
-            default = nushell;
-          }
-          // (lib.genAttrs pluginPackageNames (
-            package:
+        {
+          inherit (packages) nushell nushellFull;
+          nushellPlugins =
+            let
+              pluginPkgs = lib.filterAttrs (name: _: lib.hasPrefix "nu_plugin_" name) packages;
+            in
+            lib.mapAttrs' (
+              name: value: lib.nameValuePair (lib.removePrefix "nu_plugin_" name) value
+            ) pluginPkgs;
+        };
+
+      perSystem =
+        { pkgs, lib, ... }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
+
+          devShells.default = pkgs.mkShell { packages = builtins.attrValues { inherit (pkgs) npins; }; };
+          packages =
+            let
+              plugins = [
+                "formats"
+                "gstat"
+                "inc"
+                "query"
+              ];
+              pluginPackageNames = map (p: "nu_plugin_${p}") plugins;
+              nushell = pkgs.callPackage ./nushell.nix {
+                doCheck = false;
+                inherit (pkgs.darwin.apple_sdk_11_0) Libsystem;
+                inherit (pkgs.darwin.apple_sdk_11_0.frameworks) AppKit Security;
+              };
+            in
+            {
+              inherit nushell;
+              nushellFull = nushell.override { additionalFeatures = default: (default ++ [ "dataframe" ]); };
+              default = nushell;
+            }
+            // (lib.genAttrs pluginPackageNames (
+              package:
               nushell.override {
                 inherit package;
                 pname = package;
               }
-          ));
+            ));
 
-        apps = let
-          mkApp = app: {
-            type = "app";
-            program = pkgs.lib.getExe app;
-          };
-        in {
-          update = let
-            sources = import ./npins;
-            inherit (sources) nushell;
-          in
-            mkApp (pkgs.writeShellApplication {
-              name = "update";
-              runtimeInputs = builtins.attrValues {inherit (pkgs) npins jq git;};
-              text = ''
-                BEFORE="${nushell.revision}"
-                npins update
-                AFTER="$(jq -j .pins.nushell.revision ./npins/sources.json)"
+          apps =
+            let
+              mkApp = app: {
+                type = "app";
+                program = pkgs.lib.getExe app;
+              };
+            in
+            {
+              update =
+                let
+                  sources = import ./npins;
+                  inherit (sources) nushell;
+                in
+                mkApp (
+                  pkgs.writeShellApplication {
+                    name = "update";
+                    runtimeInputs = builtins.attrValues { inherit (pkgs) npins jq git; };
+                    text = ''
+                      BEFORE="${nushell.revision}"
+                      npins update
+                      AFTER="$(jq -j .pins.nushell.revision ./npins/sources.json)"
 
 
-                if [ "$BEFORE" != "$AFTER" ]; then
-                    git commit -a -m "Nushell: $BEFORE -> $AFTER"
-                fi
-              '';
-            });
+                      if [ "$BEFORE" != "$AFTER" ]; then
+                          git commit -a -m "Nushell: $BEFORE -> $AFTER"
+                      fi
+                    '';
+                  }
+                );
+            };
         };
-      };
     };
 }
--- nushell.nix
+++ nushell.nix
@@ -20,70 +20,76 @@
   pname ? "nushell",
   testers,
   nix-update-script,
-}: let
+}:
+let
   sources = import ./npins;
   inherit (sources) nushell;
 in
-  rustPlatform.buildRustPackage {
-    inherit pname doCheck;
-    version = nushell.revision;
-    src = nushell;
+rustPlatform.buildRustPackage {
+  inherit pname doCheck;
+  version = nushell.revision;
+  src = nushell;
 
-    buildNoDefaultFeatures = !withDefaultFeatures;
+  buildNoDefaultFeatures = !withDefaultFeatures;
 
-    # Our source doesn't contain a .git folder from which to extract the hash,
-    # so the build script is patched to fallback to our own hash instead of an empty string.
-    # This is only relevant for showing the current commit when running the `version` command
-    patchPhase = ''
-      runHook prePatch
+  # Our source doesn't contain a .git folder from which to extract the hash,
+  # so the build script is patched to fallback to our own hash instead of an empty string.
+  # This is only relevant for showing the current commit when running the `version` command
+  patchPhase = ''
+    runHook prePatch
 
-      substituteInPlace "crates/nu-cmd-lang/build.rs" --replace-fail "get_git_hash().unwrap_or_default();" "get_git_hash().unwrap_or(\"${nushell.revision}\".into());"
-      runHook postPatch
-    '';
+    substituteInPlace "crates/nu-cmd-lang/build.rs" --replace-fail "get_git_hash().unwrap_or_default();" "get_git_hash().unwrap_or(\"${nushell.revision}\".into());"
+    runHook postPatch
+  '';
 
-    cargoLock = {
-      lockFile = "${nushell}/Cargo.lock";
-      allowBuiltinFetchGit = true;
-    };
-    nativeBuildInputs =
-      [pkg-config]
-      ++ lib.optionals (withDefaultFeatures && stdenv.isLinux) [python3]
-      ++ lib.optionals stdenv.isDarwin [rustPlatform.bindgenHook];
+  cargoLock = {
+    lockFile = "${nushell}/Cargo.lock";
+    allowBuiltinFetchGit = true;
+  };
+  nativeBuildInputs =
+    [ pkg-config ]
+    ++ lib.optionals (withDefaultFeatures && stdenv.isLinux) [ python3 ]
+    ++ lib.optionals stdenv.isDarwin [ rustPlatform.bindgenHook ];
 
-    buildInputs =
-      [openssl zstd]
-      ++ lib.optionals stdenv.isDarwin [
-        zlib
-        Libsystem
-        Security
-      ]
-      ++ lib.optionals (withDefaultFeatures && stdenv.isLinux) [xorg.libX11]
-      ++ lib.optionals (withDefaultFeatures && stdenv.isDarwin) [AppKit nghttp2 libgit2];
+  buildInputs =
+    [
+      openssl
+      zstd
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      zlib
+      Libsystem
+      Security
+    ]
+    ++ lib.optionals (withDefaultFeatures && stdenv.isLinux) [ xorg.libX11 ]
+    ++ lib.optionals (withDefaultFeatures && stdenv.isDarwin) [
+      AppKit
+      nghttp2
+      libgit2
+    ];
 
-    checkPhase = ''
-      runHook preCheck
-      echo "Running cargo test"
-      HOME=$(mktemp -d) cargo test
-      runHook postCheck
-    '';
+  checkPhase = ''
+    runHook preCheck
+    echo "Running cargo test"
+    HOME=$(mktemp -d) cargo test
+    runHook postCheck
+  '';
 
-    passthru = {
-      shellPath = "/bin/nu";
-      tests.version = testers.testVersion {
-        package = nushell;
-      };
-      updateScript = nix-update-script {};
-    };
+  passthru = {
+    shellPath = "/bin/nu";
+    tests.version = testers.testVersion { package = nushell; };
+    updateScript = nix-update-script { };
+  };
 
-    cargoBuildFlags = ["--package ${package}"];
+  cargoBuildFlags = [ "--package ${package}" ];
 
-    buildFeatures = additionalFeatures [];
+  buildFeatures = additionalFeatures [ ];
 
-    meta = with lib; {
-      description = "A modern shell written in Rust";
-      homepage = "https://www.nushell.sh/";
-      license = licenses.mit;
-      maintainers = [];
-      mainProgram = package;
-    };
-  }
+  meta = with lib; {
+    description = "A modern shell written in Rust";
+    homepage = "https://www.nushell.sh/";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = package;
+  };
+}
